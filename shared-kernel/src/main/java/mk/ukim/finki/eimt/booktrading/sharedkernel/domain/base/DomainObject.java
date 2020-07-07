68d41e129f6ec67a2ae17c5b2cf6f5af198e257c
Egzon Iseini <egzon.iseini@sorsix.com>
2020-07-07 21:59:27 +0200

    Added shared kernel code.

---
+++ shared-kernel/src/main/java/mk/ukim/finki/eimt/booktrading/sharedkernel/domain/base/DomainObject.java
@@ -0,0 +1,6 @@
+package mk.ukim.finki.eimt.booktrading.sharedkernel.domain.base;
+
+import java.io.Serializable;
+
+public interface DomainObject extends Serializable {
+}
---
+++ shared-kernel/src/main/java/mk/ukim/finki/eimt/booktrading/sharedkernel/domain/base/ValueObject.java
@@ -0,0 +1,4 @@
+package mk.ukim.finki.eimt.booktrading.sharedkernel.domain.base;
+
+public interface ValueObject extends DomainObject{
+}

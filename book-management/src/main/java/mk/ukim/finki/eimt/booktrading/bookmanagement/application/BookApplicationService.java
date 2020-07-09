18212dcbad5e401f8379980551b86ed0ac7559e6
Egzon Iseini <egzon.iseini@sorsix.com>
2020-07-10 00:14:00 +0200

    Added CRUD for Books.

--- book-management/src/main/java/mk/ukim/finki/eimt/booktrading/bookmanagement/application/BookApplicationService.java
+++ book-management/src/main/java/mk/ukim/finki/eimt/booktrading/bookmanagement/application/BookApplicationService.java
@@ -1,9 +1,14 @@
 package mk.ukim.finki.eimt.booktrading.bookmanagement.application;
 
 import mk.ukim.finki.eimt.booktrading.bookmanagement.domain.model.Book;
+import mk.ukim.finki.eimt.booktrading.bookmanagement.domain.model.BookId;
 import mk.ukim.finki.eimt.booktrading.bookmanagement.domain.model.dto.CreateNewBookRequestDto;
 import mk.ukim.finki.eimt.booktrading.bookmanagement.domain.repository.BookRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookApplicationService {
@@ -19,6 +24,18 @@ public class BookApplicationService {
     public Book createNewBook(CreateNewBookRequestDto createNewBookRequestDto) {
         Book newBook = Book.valueOf(createNewBookRequestDto);
         return bookRepository.save(newBook);
+    }
+
+    public Book getBookById(BookId bookId) {
+        return bookRepository.findById(bookId).orElseThrow(RuntimeException::new);
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Page<Book> getBookPageable(Pageable pageable) {
+        return bookRepository.findAll(pageable);
     }
 
 }
--- book-management/src/main/java/mk/ukim/finki/eimt/booktrading/bookmanagement/controller/BookController.java
+++ book-management/src/main/java/mk/ukim/finki/eimt/booktrading/bookmanagement/controller/BookController.java
@@ -2,11 +2,14 @@ package mk.ukim.finki.eimt.booktrading.bookmanagement.controller;
 
 import mk.ukim.finki.eimt.booktrading.bookmanagement.application.BookApplicationService;
 import mk.ukim.finki.eimt.booktrading.bookmanagement.domain.model.Book;
+import mk.ukim.finki.eimt.booktrading.bookmanagement.domain.model.BookId;
 import mk.ukim.finki.eimt.booktrading.bookmanagement.domain.model.dto.CreateNewBookRequestDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/book")
@@ -18,6 +21,21 @@ public class BookController {
             BookApplicationService bookApplicationService
     ) {
         this.bookApplicationService = bookApplicationService;
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookApplicationService.getAllBooks();
+    }
+
+    @GetMapping(params = {"page", "pageSize"})
+    public Page<Book> getAllBooksPageable(@RequestParam Integer page, @RequestParam Integer pageSize) {
+        return bookApplicationService.getBookPageable(PageRequest.of(page, pageSize));
+    }
+
+    @GetMapping("/{bookId}")
+    public Book getBookById(@PathVariable String bookId) {
+        return bookApplicationService.getBookById(new BookId(bookId));
     }
 
     @PostMapping
